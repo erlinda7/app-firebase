254228fec1f35a215729a41388b25821f7282a74
Erlinda <chambimanzanoerlinda@gmail.com>
2020-05-04 15:37:21 -0400

    estructura inicial del proyecto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Dashboard from "./pages/dashboard";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const root = (
+    <BrowserRouter>
+        <Switch>
+            <Route type="private" path="/dashboard" component={Dashboard} />
+            <Redirect from="/" to="/dashboard" />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(root, document.getElementById('root'));
